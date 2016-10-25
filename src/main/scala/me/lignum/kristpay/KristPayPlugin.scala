6cacb35198a6953184f93362f0172af463ce3d13
Lignum <grgmckinlay@gmail.com>
2016-10-25 22:23:28 +0200

    Add source

---
+++ src/main/scala/me/lignum/kristpay/KristPayPlugin.scala
@@ -0,0 +1,13 @@
+package me.lignum.kristpay
+
+import org.spongepowered.api.plugin.Plugin
+
+@Plugin(
+  id = "kristpay",
+  name = "KristPay",
+  version = "1.0.0",
+  authors = Array("Lignum")
+)
+class KristPayPlugin {
+
+}
